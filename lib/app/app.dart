34a9b7f8b1bfcb6f9813609d6d5ded352a8e62f5
MuhammadAbdullahIqbal23 <112970908+MuhammadAbdullahIqbal23@users.noreply.github.com>
2024-06-25 12:12:08 +0500

    New: 1. Login View 2. stripe service 3. bottom sheet alert 4. error dialog 5. user_lists widget

---
+++ lib/app/app.dart
@@ -0,0 +1,40 @@
+import 'package:my_app/ui/bottom_sheets/notice/notice_sheet.dart';
+import 'package:my_app/ui/dialogs/info_alert/info_alert_dialog.dart';
+import 'package:my_app/ui/views/home/home_view.dart';
+import 'package:my_app/ui/views/startup/startup_view.dart';
+import 'package:stacked/stacked_annotations.dart';
+import 'package:stacked_services/stacked_services.dart';
+import 'package:my_app/ui/views/login/login_view.dart';
+import 'package:my_app/ui/views/login/login_view.dart';
+import 'package:my_app/services/stripe_service.dart';
+import 'package:my_app/ui/bottom_sheets/alert/alert_sheet.dart';
+import 'package:my_app/ui/dialogs/error/error_dialog.dart';
+// @stacked-import
+
+@StackedApp(
+  routes: [
+    MaterialRoute(page: HomeView),
+    MaterialRoute(page: StartupView),
+    MaterialRoute(page: LoginView),
+    MaterialRoute(page: LoginView),
+// @stacked-route
+  ],
+  dependencies: [
+    LazySingleton(classType: BottomSheetService),
+    LazySingleton(classType: DialogService),
+    LazySingleton(classType: NavigationService),
+    LazySingleton(classType: StripeService),
+// @stacked-service
+  ],
+  bottomsheets: [
+    StackedBottomsheet(classType: NoticeSheet),
+    StackedBottomsheet(classType: AlertSheet),
+// @stacked-bottom-sheet
+  ],
+  dialogs: [
+    StackedDialog(classType: InfoAlertDialog),
+    StackedDialog(classType: ErrorDialog),
+// @stacked-dialog
+  ],
+)
+class App {}
